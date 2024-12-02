d5ddb65d8cf5119267fc7ed171166c97464d4eeb
Israt85 <isratanjuman3@gmail.com>
2024-12-02 21:31:13 +0600

    added upto section 5

--- src/Pages/Blogs/Blogs.jsx
+++ src/Pages/Blogs/Blogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BlogBanner from '../../Components/Blogs/BlogBanner';
 import Section2 from '../../Components/Blogs/Section2';
 import Section3 from '../../Components/Blogs/Section3';
+import Section4 from '../../Components/Blogs/Section4';
 
 const Blogs = () => {
     return (
@@ -9,6 +10,7 @@ const Blogs = () => {
             <BlogBanner></BlogBanner>
             <Section2></Section2>
             <Section3></Section3>
+            <Section4></Section4>
         </div>
     );
 };
